293c14256277456a7f81ddb564e590d5e6f04a14
Olli Glorioso <olli.glorioso@avanio.com>
2022-07-22 13:48:30 +0300

    Added npx script

---
+++ bin/cli.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import { execSync } from "child_process"
+
+const run = (command) => {
+    try {
+        execSync(`${command}`, { stdio: "inherit" })
+    } catch (e) {
+        console.error(`Failed to execute ${command}`, e)
+        return false
+    }
+}
+
+const folderName = process.argv[2]
+const gitCloneCmd = `git clone https://github.com/olliglorioso/typescript-npm-base.git ${folderName}`
+const installDepsCmd = `cd ${folderName} && npm i`
+
+console.log(`Cloning the repository to a folder with the name ${folderName}`)
+const cloned = run(gitCloneCmd)
+if (!cloned) process.exit(-1)
+
+console.log(`Installing devDependencies to the folder ${folderName}`)
+const installed = run(installDepsCmd)
+if (!installed) process.exit(-1)
+
+console.log("Congratulations, you have initiliazed a new Node.js with Typescript project. Run the following command to start:")
+console.log(`cd ${folderName}`)
